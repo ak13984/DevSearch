6567609510c54430ab8b9f96a9de8b4156027ec3
ak13984 <ak13984@yahoo.com>
2022-08-01 02:37:54 +0530

    Devsearch

---
+++ projects/views.py
@@ -0,0 +1,82 @@
+from django.core import paginator
+from django.shortcuts import render, redirect
+
+# Create your views here.
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+from django.db.models import Q
+from django.contrib import messages
+from .models import Project, Tag
+from .forms import ProjectForm, ReviewForm
+from .utils import searchProjects, paginateProjects
+
+def projects(request):
+
+    projects,search_query = searchProjects(request)
+    custom_range, projects = paginateProjects(request, projects, 6)
+
+    context = {'projects':projects,
+    'search_query':search_query,
+    'custom_range':custom_range}
+
+    return render(request,'projects/projects.html',context)
+
+def project(request,pk):
+    printObj=Project.objects.get(id=pk)
+    form = ReviewForm()
+    tags = printObj.tags.all()
+
+    if request.method == 'POST':
+        form = ReviewForm(request.POST)
+        review = form.save(commit=False)
+        review.project = printObj
+        review.owner = request.user.profile
+        review.save()
+        messages.success(request, 'Your review was successfully submitted!')
+        return redirect('project',pk=printObj.id)
+
+    return render(request,'projects/single-project.html',{'project':printObj,'tags':tags,'form':form})
+
+@login_required(login_url="login")
+def createProject(request):
+    profile = request.user.profile
+    form = ProjectForm()
+
+    if request.method == 'POST':
+        form = ProjectForm(request.POST, request.FILES)
+        if form.is_valid():
+            project = form.save(commit=False)
+            project.owner = profile
+            project.save()
+            return redirect('account')
+
+    context = {'form':form}
+    return render(request, "projects/project_form.html", context)
+
+
+@login_required(login_url="login")
+def updateProject(request,pk):
+    profile = request.user.profile
+    project = profile.project_set.get(id=pk)
+    form = ProjectForm(instance=project)
+
+    if request.method == 'POST':
+        form = ProjectForm(request.POST, request.FILES, instance=project)
+        if form.is_valid():
+            form.save()
+            return redirect('account')
+    context = {'form':form}
+    return render(request, "projects/project_form.html", context)
+
+
+
+@login_required(login_url="login")
+def deleteProject(request,pk):
+    profile = request.user.profile
+    project = profile.project_set.get(id=pk)
+    if request.method == 'POST':
+        project.delete()
+        return redirect('projects')
+    context = {'object':project}
+    return render(request,"delete_template.html",context)
